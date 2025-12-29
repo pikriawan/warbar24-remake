3f4b77b344a5453d6e204068025284bf01b452c7
Fikri Setiawan <fikrisetiawan2006@gmail.com>
2025-12-29 19:39:34 +0700

    Create models, migrations, and admin auth guard

---
+++ database/migrations/2025_12_29_123356_create_orders_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('admin_id')->nullable()->constrained();
+            $table->foreignId('customer_id')->constrained();
+            $table->string('order_number');
+            $table->enum('status', ['pending', 'processing', 'finished', 'canceled'])->nullable();
+            $table->double('total_payment');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
