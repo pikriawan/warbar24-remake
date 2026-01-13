57aa2851f72c73d3f4fd44b6dd14827c34688b5f
Fikri Setiawan <fikrisetiawan2006@gmail.com>
2026-01-13 13:03:42 +0700

    Change database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -27,7 +27,7 @@ class DatabaseSeeder extends Seeder
         Menu::create([
             'admin_id'     => $admin->id,
             'name'         => 'Telur Balado',
-            'price'        => 3000,
+            'price'        => 4000,
             'category'     => 'makanan',
             'is_available' => true,
             'image'        => Storage::disk('public')->putFile('uploads', new File(public_path('images/telur-balado.jpg'))),
@@ -35,11 +35,10 @@ class DatabaseSeeder extends Seeder
 
         Menu::create([
             'admin_id'     => $admin->id,
-            'name'         => 'Sayur Sop',
-            'price'        => 5000,
+            'name'         => 'Gorengan',
+            'price'        => 1000,
             'category'     => 'makanan',
             'is_available' => true,
-            'image'        => Storage::disk('public')->putFile('uploads', new File(public_path('images/sayur-sop.jpg'))),
         ]);
 
         Menu::create([
@@ -53,56 +52,346 @@ class DatabaseSeeder extends Seeder
 
         Menu::create([
             'admin_id'     => $admin->id,
-            'name'         => 'Steak',
-            'price'        => 30000,
+            'name'         => 'Nasi',
+            'price'        => 5000,
             'category'     => 'makanan',
             'is_available' => true,
-            'image'        => Storage::disk('public')->putFile('uploads', new File(public_path('images/steak.jpg'))),
+            'image'        => Storage::disk('public')->putFile('uploads', new File(public_path('images/nasi.jpg'))),
         ]);
 
         Menu::create([
             'admin_id'     => $admin->id,
-            'name'         => 'Hamburger',
-            'price'        => 40000,
+            'name'         => 'Kerupuk',
+            'price'        => 2000,
             'category'     => 'makanan',
             'is_available' => true,
-            'image'        => Storage::disk('public')->putFile('uploads', new File(public_path('images/hamburger.jpg'))),
         ]);
 
         Menu::create([
             'admin_id'     => $admin->id,
-            'name'         => 'Pasta',
-            'price'        => 25000,
+            'name'         => 'Sayur Terong',
+            'price'        => 4000,
             'category'     => 'makanan',
             'is_available' => true,
-            'image'        => Storage::disk('public')->putFile('uploads', new File(public_path('images/pasta.jpg'))),
+            'image'        => Storage::disk('public')->putFile('uploads', new File(public_path('images/sayur-terong.jpg'))),
         ]);
 
         Menu::create([
             'admin_id'     => $admin->id,
-            'name'         => 'Green Tea',
+            'name'         => 'Sayur Tahu Pedas',
+            'price'        => 2000,
+            'category'     => 'makanan',
+            'is_available' => true,
+            'image'        => Storage::disk('public')->putFile('uploads', new File(public_path('images/sayur-tahu-pedas.jpg'))),
+        ]);
+
+        Menu::create([
+            'admin_id'     => $admin->id,
+            'name'         => 'Oseng Usus',
+            'price'        => 5000,
+            'category'     => 'makanan',
+            'is_available' => true,
+        ]);
+
+        Menu::create([
+            'admin_id'     => $admin->id,
+            'name'         => 'Ati Balado',
+            'price'        => 5000,
+            'category'     => 'makanan',
+            'is_available' => true,
+        ]);
+
+        Menu::create([
+            'admin_id'     => $admin->id,
+            'name'         => 'Tumis Kacang Panjang',
+            'price'        => 3000,
+            'category'     => 'makanan',
+            'is_available' => true,
+            'image'        => Storage::disk('public')->putFile('uploads', new File(public_path('images/tumis-kacang-panjang.jpg'))),
+        ]);
+
+        Menu::create([
+            'admin_id'     => $admin->id,
+            'name'         => 'Sayur Sop',
+            'price'        => 3000,
+            'category'     => 'makanan',
+            'is_available' => true,
+            'image'        => Storage::disk('public')->putFile('uploads', new File(public_path('images/sayur-sop.jpg'))),
+        ]);
+
+        Menu::create([
+            'admin_id'     => $admin->id,
+            'name'         => 'Soto',
             'price'        => 10000,
-            'category'     => 'minuman',
-            'is_available' => false,
-            'image'        => Storage::disk('public')->putFile('uploads', new File(public_path('images/green-tea.jpg'))),
+            'category'     => 'makanan',
+            'is_available' => true,
         ]);
 
         Menu::create([
             'admin_id'     => $admin->id,
-            'name'         => 'Chocolate Milk',
-            'price'        => 15000,
+            'name'         => 'Ayam Goreng',
+            'price'        => 9000,
+            'category'     => 'makanan',
+            'is_available' => true,
+            'image'        => Storage::disk('public')->putFile('uploads', new File(public_path('images/ayam-goreng.jpg'))),
+        ]);
+
+        Menu::create([
+            'admin_id'     => $admin->id,
+            'name'         => 'Mie Goreng',
+            'price'        => 3000,
+            'category'     => 'makanan',
+            'is_available' => true,
+            'image'        => Storage::disk('public')->putFile('uploads', new File(public_path('images/mie-goreng.jpg'))),
+        ]);
+
+        Menu::create([
+            'admin_id'     => $admin->id,
+            'name'         => 'Tumis Kangkung',
+            'price'        => 3000,
+            'category'     => 'makanan',
+            'is_available' => true,
+            'image'        => Storage::disk('public')->putFile('uploads', new File(public_path('images/tumis-kangkung.jpg'))),
+        ]);
+
+        Menu::create([
+            'admin_id'     => $admin->id,
+            'name'         => 'Kikil',
+            'price'        => 3000,
+            'category'     => 'makanan',
+            'is_available' => true,
+        ]);
+
+        Menu::create([
+            'admin_id'     => $admin->id,
+            'name'         => 'Orek Basah',
+            'price'        => 3000,
+            'category'     => 'makanan',
+            'is_available' => true,
+        ]);
+
+        Menu::create([
+            'admin_id'     => $admin->id,
+            'name'         => 'Orek Kering',
+            'price'        => 3000,
+            'category'     => 'makanan',
+            'is_available' => true,
+        ]);
+
+        Menu::create([
+            'admin_id'     => $admin->id,
+            'name'         => 'Buncis',
+            'price'        => 3000,
+            'category'     => 'makanan',
+            'is_available' => true,
+        ]);
+
+        Menu::create([
+            'admin_id'     => $admin->id,
+            'name'         => 'Labu Siam',
+            'price'        => 3000,
+            'category'     => 'makanan',
+            'is_available' => true,
+        ]);
+
+        Menu::create([
+            'admin_id'     => $admin->id,
+            'name'         => 'Tumis Jamur',
+            'price'        => 3000,
+            'category'     => 'makanan',
+            'is_available' => true,
+            'image'        => Storage::disk('public')->putFile('uploads', new File(public_path('images/tumis-jamur.jpg'))),
+        ]);
+
+        Menu::create([
+            'admin_id'     => $admin->id,
+            'name'         => 'Tumis Tauge',
+            'price'        => 3000,
+            'category'     => 'makanan',
+            'is_available' => true,
+        ]);
+
+        Menu::create([
+            'admin_id'     => $admin->id,
+            'name'         => 'Tumis Pakcoy',
+            'price'        => 3000,
+            'category'     => 'makanan',
+            'is_available' => true,
+        ]);
+
+        Menu::create([
+            'admin_id'     => $admin->id,
+            'name'         => 'Telur Ceplok',
+            'price'        => 4000,
+            'category'     => 'makanan',
+            'is_available' => true,
+            'image'        => Storage::disk('public')->putFile('uploads', new File(public_path('images/telur-ceplok.jpg'))),
+        ]);
+
+        Menu::create([
+            'admin_id'     => $admin->id,
+            'name'         => 'Telur Dadar',
+            'price'        => 4000,
+            'category'     => 'makanan',
+            'is_available' => true,
+            'image'        => Storage::disk('public')->putFile('uploads', new File(public_path('images/telur-dadar.jpg'))),
+        ]);
+
+        Menu::create([
+            'admin_id'     => $admin->id,
+            'name'         => 'Pindang Goreng',
+            'price'        => 9000,
+            'category'     => 'makanan',
+            'is_available' => true,
+            'image'        => Storage::disk('public')->putFile('uploads', new File(public_path('images/pindang-goreng.jpg'))),
+        ]);
+
+        Menu::create([
+            'admin_id'     => $admin->id,
+            'name'         => 'Tumis Sawi Putih',
+            'price'        => 3000,
+            'category'     => 'makanan',
+            'is_available' => true,
+        ]);
+
+        Menu::create([
+            'admin_id'     => $admin->id,
+            'name'         => 'Ayam Balado',
+            'price'        => 9000,
+            'category'     => 'makanan',
+            'is_available' => true,
+            'image'        => Storage::disk('public')->putFile('uploads', new File(public_path('images/ayam-balado.jpg'))),
+        ]);
+
+        Menu::create([
+            'admin_id'     => $admin->id,
+            'name'         => 'Ayam Kecap',
+            'price'        => 9000,
+            'category'     => 'makanan',
+            'is_available' => true,
+            'image'        => Storage::disk('public')->putFile('uploads', new File(public_path('images/ayam-kecap.jpg'))),
+        ]);
+
+        Menu::create([
+            'admin_id'     => $admin->id,
+            'name'         => 'Kentang Balado',
+            'price'        => 3000,
+            'category'     => 'makanan',
+            'is_available' => true,
+            'image'        => Storage::disk('public')->putFile('uploads', new File(public_path('images/kentang-balado.jpg'))),
+        ]);
+
+        Menu::create([
+            'admin_id'     => $admin->id,
+            'name'         => 'Ayam Opor',
+            'price'        => 9000,
+            'category'     => 'makanan',
+            'is_available' => true,
+        ]);
+
+        Menu::create([
+            'admin_id'     => $admin->id,
+            'name'         => 'Lele Goreng',
+            'price'        => 9000,
+            'category'     => 'makanan',
+            'is_available' => true,
+            'image'        => Storage::disk('public')->putFile('uploads', new File(public_path('images/lele-goreng.jpg'))),
+        ]);
+
+        Menu::create([
+            'admin_id'     => $admin->id,
+            'name'         => 'Es Teh',
+            'price'        => 3000,
             'category'     => 'minuman',
             'is_available' => true,
-            'image'        => Storage::disk('public')->putFile('uploads', new File(public_path('images/chocolate-milk.jpg'))),
+            'image'        => Storage::disk('public')->putFile('uploads', new File(public_path('images/es-teh.jpg'))),
         ]);
 
         Menu::create([
             'admin_id'     => $admin->id,
-            'name'         => 'Coffee',
+            'name'         => 'Teh Hangat',
+            'price'        => 3000,
+            'category'     => 'minuman',
+            'is_available' => true,
+            'image'        => Storage::disk('public')->putFile('uploads', new File(public_path('images/teh-hangat.jpg'))),
+        ]);
+
+        Menu::create([
+            'admin_id'     => $admin->id,
+            'name'         => 'Es Jeruk',
             'price'        => 5000,
             'category'     => 'minuman',
             'is_available' => true,
-            'image'        => Storage::disk('public')->putFile('uploads', new File(public_path('images/coffee.jpg'))),
+            'image'        => Storage::disk('public')->putFile('uploads', new File(public_path('images/es-jeruk.jpg'))),
+        ]);
+
+        Menu::create([
+            'admin_id'     => $admin->id,
+            'name'         => 'Lemon Tea',
+            'price'        => 5000,
+            'category'     => 'minuman',
+            'is_available' => true,
+            'image'        => Storage::disk('public')->putFile('uploads', new File(public_path('images/lemon-tea.jpg'))),
+        ]);
+
+        Menu::create([
+            'admin_id'     => $admin->id,
+            'name'         => 'Nutrisari Jeruk Peras',
+            'price'        => 4000,
+            'category'     => 'minuman',
+            'is_available' => true,
+        ]);
+
+        Menu::create([
+            'admin_id'     => $admin->id,
+            'name'         => 'Air Es',
+            'price'        => 2000,
+            'category'     => 'minuman',
+            'is_available' => true,
+        ]);
+
+        Menu::create([
+            'admin_id'     => $admin->id,
+            'name'         => 'Kopi Hitam',
+            'price'        => 4000,
+            'category'     => 'minuman',
+            'is_available' => true,
+            'image'        => Storage::disk('public')->putFile('uploads', new File(public_path('images/kopi-hitam.jpg'))),
+        ]);
+
+        Menu::create([
+            'admin_id'     => $admin->id,
+            'name'         => 'Susu Putih Hangat',
+            'price'        => 4000,
+            'category'     => 'minuman',
+            'is_available' => true,
+            'image'        => Storage::disk('public')->putFile('uploads', new File(public_path('images/susu-putih-hangat.jpg'))),
+        ]);
+
+        Menu::create([
+            'admin_id'     => $admin->id,
+            'name'         => 'Es Susu Putih',
+            'price'        => 5000,
+            'category'     => 'minuman',
+            'is_available' => true,
+        ]);
+
+        Menu::create([
+            'admin_id'     => $admin->id,
+            'name'         => 'Susu Cokelat Hangat',
+            'price'        => 4000,
+            'category'     => 'minuman',
+            'is_available' => true,
+            'image'        => Storage::disk('public')->putFile('uploads', new File(public_path('images/susu-cokelat-hangat.jpg'))),
+        ]);
+
+        Menu::create([
+            'admin_id'     => $admin->id,
+            'name'         => 'Es Susu Cokelat',
+            'price'        => 5000,
+            'category'     => 'minuman',
+            'is_available' => true,
+            'image'        => Storage::disk('public')->putFile('uploads', new File(public_path('images/es-susu-cokelat.jpg'))),
         ]);
     }
 }
--- resources/views/menus.blade.php
+++ resources/views/menus.blade.php
@@ -2,28 +2,32 @@
     @push('styles')
         @vite('resources/css/menus.css')
     @endpush
-    <div class="menu-category-list">
-        @foreach ($data as $category => $menus)
-            <div>
-                <h2>{{ $category === '' ? 'Uncategorized' : Str::title($category) }}</h2>
-                <div class="menu-list">
-                    @foreach ($menus as $menu)
-                        <div>
-                            <img class="menu-image" src="{{ $menu->image === null ? '/images/menu-no-image.png' : asset('storage/' . $menu->image) }}" alt="{{ $menu->name }}" width="128" height="128">
-                            <h3>{{ $menu->name }}</h3>
-                            <p>Harga: {{ $menu->price }}</p>
-                            <p>Kategori: {{ $menu->category === null ? 'uncategorized' : $menu->category }}</p>
-                            <p>{{ $menu->is_available ? 'Tersedia' : 'Habis' }}</p>
-                            <form action="/cart" method="post">
-                                @csrf
-                                <input type="hidden" name="menu_id" value="{{ $menu->id }}">
-                                <input type="hidden" name="action" value="increment">
-                                <button>Tambah</button>
-                            </form>
-                        </div>
-                    @endforeach
+    @if (count($data) > 0)
+        <div class="menu-category-list">
+            @foreach ($data as $category => $menus)
+                <div>
+                    <h2>{{ $category === '' ? 'Uncategorized' : Str::title($category) }}</h2>
+                    <div class="menu-list">
+                        @foreach ($menus as $menu)
+                            <div>
+                                <img class="menu-image" src="{{ $menu->image === null ? '/images/menu-no-image.png' : asset('storage/' . $menu->image) }}" alt="{{ $menu->name }}" width="128" height="128">
+                                <h3>{{ $menu->name }}</h3>
+                                <p>Harga: {{ $menu->price }}</p>
+                                <p>Kategori: {{ $menu->category === null ? 'uncategorized' : $menu->category }}</p>
+                                <p>{{ $menu->is_available ? 'Tersedia' : 'Habis' }}</p>
+                                <form action="/cart" method="post">
+                                    @csrf
+                                    <input type="hidden" name="menu_id" value="{{ $menu->id }}">
+                                    <input type="hidden" name="action" value="increment">
+                                    <button>Tambah</button>
+                                </form>
+                            </div>
+                        @endforeach
+                    </div>
                 </div>
-            </div>
-        @endforeach
-    </div>
+            @endforeach
+        </div>
+    @else
+        <p>Tidak ada menu</p>
+    @endif
 </x-customer-layout>
