4eddb1bd9c5d030b99073bde7263ce8386721c74
Fikri Setiawan <fikrisetiawan2006@gmail.com>
2026-01-21 19:35:46 +0700

    Redesign admin login page

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -1,18 +1,24 @@
+@push('styles')
+    @vite('resources/css/admin-login.css')
+@endpush
 <x-root-layout>
-    <h1>Login</h1>
-    <form action="/admin/login" method="post">
-        @csrf
-        <div>
-            <label for="username">Username</label>
-            <input id="username" name="username" required>
-        </div>
-        <div>
-            <label for="password">Password</label>
-            <input type="password" id="password" name="password" required>
-        </div>
-        <button>Login</button>
-        @error('login')
-            <p>Username atau password salah</p>
-        @enderror
-    </form>
+    <main class="main">
+        <img src="/images/warteg-bahari-24.svg" alt="Warteg Bahari 24" width="128">
+        <h1 class="login-title">Login</h1>
+        <form class="login-form" action="/admin/login" method="post">
+            @csrf
+            <div class="form-field">
+                <label class="label" for="username">Username</label>
+                <input class="text-field" id="username" name="username" placeholder="Username Anda" required>
+            </div>
+            <div class="form-field">
+                <label class="label" for="password">Password</label>
+                <input class="text-field" type="password" id="password" name="password" placeholder="Password Anda" required>
+            </div>
+            @error('login')
+                <p class="error-text">Username atau password salah</p>
+            @enderror
+            <button class="button button-primary">Login</button>
+        </form>
+    </main>
 </x-root-layout>
