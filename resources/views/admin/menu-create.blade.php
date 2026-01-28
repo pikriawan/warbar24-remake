c1afda60056fa625504353eab8140a3e04a02f1f
Fikri Setiawan <fikrisetiawan2006@gmail.com>
2026-01-28 20:58:50 +0700

    Redesign admin menu create page

--- resources/views/admin/menu-create.blade.php
+++ resources/views/admin/menu-create.blade.php
@@ -21,40 +21,45 @@
 @endpush
 <x-admin-layout>
     <div class="menu-create-container">
-        <a class="back-link" href="/admin/menus">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-left-icon lucide-chevron-left"><path d="m15 18-6-6 6-6"/></svg>
-        </a>
-        <h1 class="menu-create-title">Buat Menu Baru</h1>
+        <div class="menu-create-header">
+            <a class="back-link" href="/admin/menus">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-left-icon lucide-chevron-left"><path d="m15 18-6-6 6-6"/></svg>
+            </a>
+            <h1 class="menu-create-title">Buat Menu Baru</h1>
+        </div>
         <form class="menu-create-form" action="/admin/menu/create" method="post" enctype="multipart/form-data">
             @csrf
             <div class="form-field">
                 <label class="label" for="name">Nama menu</label>
-                <input class="text-field" name="name" id="name" placeholder="Nama menu">
+                <input class="text-field" name="name" id="name" placeholder="Nama menu" required>
             </div>
             <div class="form-field">
-                <img class="preview" id="preview" src="/images/menu-no-image.png" alt="" width="128" height="128">
-                <label class="label" for="image">Gambar</label>
+                <img class="preview" id="preview" src="/images/menu-no-image.png" alt="" width="200" height="200">
                 <div class="button-group">
+                    <label class="button button-primary" for="imageInput">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-upload-icon lucide-upload"><path d="M12 3v12"/><path d="m17 8-5-5-5 5"/><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"/></svg>
+                        Upload gambar
+                    </label>
                     <input class="image-input" type="file" name="image" id="imageInput" accept="image/jpeg, image/png">
                     <button class="button button-outlined" id="deleteImageButton" type="button">Hapus gambar</button>
                 </div>
             </div>
             <div class="form-field">
                 <label class="label" for="price">Harga</label>
-                <input class="text-field" type="number" name="price" id="price" placeholder="10000">
+                <input class="text-field" type="number" name="price" id="price" placeholder="10000" required>
             </div>
-            <div>
+            <div class="form-field">
                 <label class="label" for="category">Kategori</label>
-                <input class="text-field" name="category" id="category">
+                <input class="text-field" name="category" id="category" placeholder="makanan">
             </div>
-            <div>
+            <div class="form-field">
                 <span class="label">Tersedia?</span>
                 <div class="radio-item">
-                    <input type="radio" name="is_available" id="available" value="true" checked>
+                    <input class="radio-input" type="radio" name="is_available" id="available" value="true" checked required>
                     <label for="available">Ya</label>
                 </div>
                 <div class="radio-item">
-                    <input type="radio" name="is_available" id="unavailable" value="false">
+                    <input class="radio-input" type="radio" name="is_available" id="unavailable" value="false" required>
                     <label for="unavailable">Tidak</label>
                 </div>
             </div>
@@ -63,9 +68,9 @@
                 <button class="button button-primary">Buat menu baru</button>
             </div>
             @if ($errors->any())
-                <ul>
+                <div class="errors">
                     @foreach ($errors->all() as $error)
-                        <li>{{ $error }}</li>
+                        <p class="error-text">{{ $error }}</p>
                     @endforeach
                 </ul>
             @endif
