00c83835f1031c0ad654341e4b6668e2f6b03713
Fikri Setiawan <fikrisetiawan2006@gmail.com>
2026-01-30 21:02:59 +0700

    Redesign admin orders page

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -25,7 +25,7 @@
                 <div class="card-icon-container card-icon-container-blue">
                     <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="#0066FF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-circle-dollar-sign-icon lucide-circle-dollar-sign"><circle cx="12" cy="12" r="10"/><path d="M16 8h-6a2 2 0 1 0 0 4h4a2 2 0 1 1 0 4H8"/><path d="M12 18V6"/></svg>
                 </div>
-                <p class="card-text">Total Pesanan</p>
+                <p class="card-text">Total Pesanan Selesai</p>
                 <p class="card-display-text">{{ $totalSales }}</p>
             </div>
             <div class="card">
--- resources/views/admin/orders.blade.php
+++ resources/views/admin/orders.blade.php
@@ -1,55 +1,114 @@
+@php
+    function formatRupiah($number) {
+        if (floor($number === $number)) {
+            return 'Rp ' . number_format($number, 0, ',', '.') . ',-';
+        } else {
+            return 'Rp ' . number_format($number, 2, ',', '.');
+        }
+    }
+
+    function getTotalPrice($order) {
+        return $order->menus->reduce(function (?float $carry, $menu) {
+            return $carry + $menu->price * $menu->pivot->menu_quantity;
+        });
+    }
+@endphp
 @push('styles')
     @vite('resources/css/admin-orders.css')
 @endpush
 <x-admin-layout>
-    <h1>Daftar pesanan</h1>
-    @if (count($orders) > 0)
-        <table>
-            <thead>
-                <tr>
-                    <th class="table-header">No. pesanan</th>
-                    <th class="table-header">Nama pelanggan</th>
-                    <th class="table-header">Tanggal pemesanan</th>
-                    <th class="table-header">Status pesanan</th>
-                    <th class="table-header">Aksi</th>
-                </tr>
-            </thead>
-            <tbody>
+    <div class="orders-container">
+        <h1 class="orders-title">Pesanan</h1>
+        @if (count($orders) > 0)
+            <div class="orders-table">
+                <div class="table-data">
+                    <p class="table-text-strong">Nomor Pesanan</p>
+                </div>
+                <div class="table-data">
+                    <p class="table-text-strong">Nama Pelanggan</p>
+                </div>
+                <div class="table-data">
+                    <p class="table-text-strong">Tanggal Pemesanan</p>
+                </div>
+                <div class="table-data">
+                    <p class="table-text-strong">Total Harga</p>
+                </div>
+                <div class="table-data">
+                    <p class="table-text-strong">Status Pesanan</p>
+                </div>
+                <div class="table-data">
+                    <p class="table-text-strong">Aksi</p>
+                </div>
                 @foreach ($orders as $order)
-                    <tr>
-                        <td class="table-data">{{ $order->id }}</td>
-                        <td class="table-data">{{ $order->customer_name }}</td>
-                        <td class="table-data">{{ $order->created_at }}</td>
-                        <td class="table-data">{{ $order->status }}</td>
-                        <td class="table-data">
-                            <a href="/admin/order/{{ $order->id }}">Detail</a>
+                    <div class="table-data">
+                        <p class="table-text">{{ $order->id }}</p>
+                    </div>
+                    <div class="table-data">
+                        <p class="table-text">{{ $order->customer_name }}</p>
+                    </div>
+                    <div class="table-data">
+                        <p class="table-text">{{ $order->checked_out_at->setTimezone('Asia/Jakarta')->format('d F Y, H:i:s') }}</p>
+                    </div>
+                    <div class="table-data">
+                        <p class="table-text">{{ formatRupiah(getTotalPrice($order)) }}</p>
+                    </div>
+                    <div class="table-data">
+                        @switch($order->status)
+                            @case('pending')
+                                <p class="table-text table-text-danger">Belum diproses</p>
+                                @break
+                            @case('processing')
+                                <p class="table-text table-text-warning">Sedang diproses</p>
+                                @break
+                            @case('finished')
+                                <p class="table-text table-text-primary">Selesai</p>
+                                @break
+                            @case('canceled')
+                                <p class="table-text table-text-primary">Dibatalkan</p>
+                        @endswitch
+                    </div>
+                    <div class="table-data">
+                        <div class="button-group">
+                            <a class="button button-outlined" href="/admin/order/{{ $order->id }}">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-eye-icon lucide-eye"><path d="M2.062 12.348a1 1 0 0 1 0-.696 10.75 10.75 0 0 1 19.876 0 1 1 0 0 1 0 .696 10.75 10.75 0 0 1-19.876 0"/><circle cx="12" cy="12" r="3"/></svg>
+                                Lihat rincian
+                            </a>
                             @if ($order->status === 'pending')
-                                <form action="/admin/order/{{ $order->id }}/status" method="post">
+                                <form class="form" action="/admin/order/{{ $order->id }}/status" method="post">
                                     @csrf
                                     @method('put')
                                     <input type="hidden" name="status" value="processing">
-                                    <button>Tandai sebagai diproses</button>
+                                    <button class="button button-primary">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-check-icon lucide-check"><path d="M20 6 9 17l-5-5"/></svg>
+                                        Tandai diproses
+                                    </button>
                                 </form>
-                                <form action="/admin/order/{{ $order->id }}/status" method="post">
+                                <form class="form" action="/admin/order/{{ $order->id }}/status" method="post">
                                     @csrf
                                     @method('put')
                                     <input type="hidden" name="status" value="canceled">
-                                    <button>Tandai sebagai dibatalkan</button>
+                                    <button class="button button-danger">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-x-icon lucide-x"><path d="M18 6 6 18"/><path d="m6 6 12 12"/></svg>
+                                        Tandai dibatalkan
+                                    </button>
                                 </form>
                             @elseif ($order->status === 'processing')
-                                <form action="/admin/order/{{ $order->id }}/status" method="post">
+                                <form class="form" action="/admin/order/{{ $order->id }}/status" method="post">
                                     @csrf
                                     @method('put')
                                     <input type="hidden" name="status" value="finished">
-                                    <button>Tandai sebagai selesai</button>
+                                    <button class="button button-primary">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-check-icon lucide-check"><path d="M20 6 9 17l-5-5"/></svg>
+                                        Tandai selesai
+                                    </button>
                                 </form>
                             @endif
-                        </td>
-                    </tr>
+                        </div>
+                    </div>
                 @endforeach
-            </tbody>
-        </table>
-    @else
-        <p>Tidak ada pesanan</p>
-    @endif
+            </div>
+        @else
+            <p class="no-order">Tidak ada pesanan</p>
+        @endif
+    </div>
 </x-admin-layout>
