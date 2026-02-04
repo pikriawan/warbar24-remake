99e5fe2c9f0efd7c91e2f7d5bf83d509fd03e9b3
Fikri Setiawan <fikrisetiawan2006@gmail.com>
2026-02-04 11:51:59 +0700

    Change admin menus page to order menus by category

--- app/Http/Controllers/Admin/MenuController.php
+++ app/Http/Controllers/Admin/MenuController.php
@@ -12,7 +12,7 @@ class MenuController extends Controller
     public function index()
     {
         return view('admin.menus', [
-            'menus' => Menu::all(),
+            'menus' => Menu::orderBy('category')->latest()->get(),
         ]);
     }
 
